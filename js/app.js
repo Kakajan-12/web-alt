b093eb0a2b2ed7d5d4b70b5c24d5758351d406d3
Kakajan <kakajantayjikov@gmail.com>
2022-05-19 18:15:32 +0500

    test

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const headerOpen = document.querySelector('.header');
 const headerClass = document.querySelector('.header__nav');
 
 if (iconMenu) {
-    iconMenu.addEventListener("click", ()=> {
+    iconMenu.addEventListener("click", () => {
         iconMenu.classList.toggle('opened');
         bodyLock.classList.toggle('lock');
         headerOpen.classList.toggle('opened');
@@ -15,8 +15,32 @@ if (iconMenu) {
 const faqs = document.querySelectorAll('.step__item');
 const faqsBtn = document.querySelectorAll('.more__info-btn');
 
-faqs.forEach((faq) => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("opened");
+if (window.innerWidth <= 767) {
+    faqs.forEach((faq) => {
+        faq.addEventListener("click", () => {
+            faq.classList.toggle("opened");
+        });
     });
-});
+}
+
+const moreBtn = document.querySelectorAll('.step__btn');
+const modalClose = document.querySelectorAll('.more__close-btn');
+
+
+if (window.innerWidth >= 767) {
+    moreBtn.forEach((btn, i) => {
+        const modal = document.getElementById(i);
+        btn.addEventListener("click", () => {
+            modal.classList.add("show");
+            bodyLock.classList.add('lock');
+        });
+    });
+
+    modalClose.forEach((btnClose, i) => {
+        const modal = document.getElementById(i);
+        btnClose.addEventListener("click", () => {
+            modal.classList.remove("show");
+            bodyLock.classList.remove('lock');
+        });
+    });
+}
