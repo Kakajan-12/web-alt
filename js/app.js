22f1e68044f0bf062af94e3566903b0ef4de3c65
Kakajan <kakajantayjikov@gmail.com>
2022-06-21 16:46:32 +0500

    done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const iconMenu = document.querySelector('.menu__button span');
+const iconMenu = document.querySelector('.header__mobile-btn span');
 const bodyLock = document.querySelector('body');
 const headerOpen = document.querySelector('.header');
 const headerClass = document.querySelector('.header__nav');
@@ -12,10 +12,10 @@ if (iconMenu) {
     });
 }
 
-const faqs = document.querySelectorAll('.step__item');
-const faqsBtn = document.querySelectorAll('.more__info-btn');
+const faqs = document.querySelectorAll('.work__item');
+const faqsBtn = document.querySelectorAll('.work__info-btn');
 
-if (window.innerWidth <= 767) {
+if (window.innerWidth <= 1400) {
     faqs.forEach((faq) => {
         faq.addEventListener("click", () => {
             faq.classList.toggle("opened");
@@ -23,11 +23,11 @@ if (window.innerWidth <= 767) {
     });
 }
 
-const moreBtn = document.querySelectorAll('.step__btn');
-const modalClose = document.querySelectorAll('.more__close-btn');
+const moreBtn = document.querySelectorAll('.work__btn');
+const modalClose = document.querySelectorAll('.work__close-btn');
 
 
-if (window.innerWidth >= 767) {
+if (window.innerWidth >= 1400) {
     moreBtn.forEach((btn, i) => {
         const modal = document.getElementById(i);
         btn.addEventListener("click", () => {
@@ -44,3 +44,32 @@ if (window.innerWidth >= 767) {
         });
     });
 }
+
+
+
+const subBody = document.querySelectorAll('.features__sub-body');
+const linkItem = document.querySelectorAll('.features__link-item');
+
+linkItem.forEach((link, index) => {
+    link.addEventListener("click", () => {
+        subBody.forEach((sub) => {
+            sub.classList.remove("active");
+        });
+        linkItem.forEach((link) => {
+            link.classList.remove("active");
+        });
+        linkItem[index].classList.add("active");
+        subBody[index].classList.add("active");
+    });
+});
+
+const caseItem = document.querySelectorAll('.cases__item');
+
+caseItem.forEach(item => {
+    item.addEventListener("click", () => {
+        caseItem.forEach(item => {
+            item.classList.remove("active");
+        });
+        item.classList.add("active");
+    });
+});
